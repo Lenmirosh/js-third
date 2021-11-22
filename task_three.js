5ab16a9e7c7312c1b1ac2924aaac7f48f11c7918
Helen Miroshnychenko <lenmirosh@ukr.net>
2021-11-22 20:50:17 +0200

    Решает задачу 3

---
+++ task_three.js
@@ -0,0 +1,14 @@
+document.write(`Задача 3`);
+       
+
+
+    for (let i = 0; i < array.length; i++ ) {
+        if (i%2 ==0) { 
+document.write(`<div class = "gray">${array[i]}</div>`);
+                      } else {
+document.write(`<div>${array[i]}</div>`);
+                             }
+                                            }
+
+       
+   
