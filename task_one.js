3c1cec049d0b2541aa735b6a05d6996a3a124e77
Helen Miroshnychenko <lenmirosh@ukr.net>
2021-11-22 20:49:55 +0200

    Решает задачу 1

---
+++ task_one.js
@@ -0,0 +1,18 @@
+ document.write(`Задача 1`); 
+     const array = [
+            'каждый',
+            'охотник',
+            'желает',
+            'знать',
+            'где',
+            'сидит',
+            'фазан' 
+                   ];
+
+        for (let i = array.length - 1; i >= 0; i--)
+        {
+ document.write(`<div class = "red">${array[i]}</div>`);
+        }
+     
+    
+    
